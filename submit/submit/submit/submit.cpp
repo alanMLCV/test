c7f920ec65a8874b00274aeda7b045d247c0aa3c
wul <wul@hiscene.com>
2015-11-30 13:41:56 +0800

    submit

---
+++ submit/submit/submit/submit.cpp
@@ -0,0 +1,13 @@
+// submit.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "hello word" << endl;
+	return 0;
+}
+
